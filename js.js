c054baade0abd1808a45f95213fdec81b76c5b15
Louarn Mathis <mathislouarn@gmail.com>
2022-12-12 11:18:27 +0100

    menu navigation fini

--- js.js
+++ js.js
@@ -2,16 +2,6 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
