766e6e051cccddfa816551ac9ff04a1ae3122d76
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2022-12-16 18:37:51 +0100

    formulaire avec JS (normalement)

--- js.js
+++ js.js
@@ -6,5 +6,17 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-const switchElement = document.querySelector('.switch input');
-const bodyElement = document.querySelector('body');
+// Récupère les données du formulaire et les envoies par mail.
+const form = document.querySelector('#form');
+form.addEventListener('submit', event => {
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const gender = form.elements.gender.value;
+  // Compose the email
+  const subject = 'Avis laissé sur le site du jeu de la vie';
+  const body = `Nom: ${name}\nEmail: ${email}\nGenre: ${gender}\n\n${message}`;
+  const mailto = `mailto:clementdepertuis@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  // Open the email client with the email pre-filled
+  window.location.href = mailto;
+});
