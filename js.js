ebe1e06992ef0ddef3b9009952afc70b78f9dfb9
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2022-12-16 21:40:18 +0100

    Fix le formulaire

--- js.js
+++ js.js
@@ -6,17 +6,13 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-// Récupère les données du formulaire et les envoies par mail.
-const form = document.querySelector('#form');
-form.addEventListener('submit', event => {
-  const name = form.elements.name.value;
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
-  const gender = form.elements.gender.value;
-  // Compose the email
-  const subject = 'Avis laissé sur le site du jeu de la vie';
-  const body = `Nom: ${name}\nEmail: ${email}\nGenre: ${gender}\n\n${message}`;
-  const mailto = `mailto:clementdepertuis@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-  // Open the email client with the email pre-filled
-  window.location.href = mailto;
-});
+function dark(){
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+function mail(){
+  var message = document.querySelector("#" + form_id_js + " [name='text']").value;
+  data_js['text'] = message;
+  window.open('mailto:clement.salomon@etu.univ-amu.fr?subject=Avis sur le site du jeu de la vie&body='+message);
+}
